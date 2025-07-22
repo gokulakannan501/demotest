0d9478430671b2c9a2da9ba0c3aaf53d9109c584
gokulakannan501 <99109761+gokulakannan501@users.noreply.github.com>
2025-07-22 11:00:00 +0530

    Update testbase.java

--- src/Testcases/testbase.java
+++ src/Testcases/testbase.java
@@ -3,7 +3,6 @@ package Testcases;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
